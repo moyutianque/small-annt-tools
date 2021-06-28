f43d6e2a70da8f517771af70711f5dcff760b356
wangzehao <wangzehao@sensetime.com>
2021-06-28 10:00:00 +0800

    Add two borderline button

--- pyqt_annt.py
+++ pyqt_annt.py
@@ -5,6 +5,9 @@ from PyQt5.QtWidgets import QVBoxLayout, QWidget, QLabel, QPushButton, QApplicat
 
 import jsonlines
 import os.path as osp
+import os
+
+import time
 
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
@@ -16,7 +19,10 @@ class ImageViewer(QWidget):
         QWidget.__init__(self)
         self.setup_ui()
         self.annt_path = annt_path
-        self.out_file = out_file
+
+        out_root = f'./annotator_choice_{int(time.time())}'
+        os.makedirs(out_root, exist_ok=True)
+        self.out_file = osp.join(out_root, out_file)
         self.reader = jsonlines.open(osp.join(annt_path, 'annt.jsonl'))
         
     def setup_ui(self):
@@ -25,16 +31,27 @@ class ImageViewer(QWidget):
 
         self.main_layout = QVBoxLayout(self)  # adding widgets to layot
         self.main_layout.addWidget(self.image_label)
-        self.button = QPushButton("Matched")
+
+        # match button
+        self.button = QPushButton("Match")
         self.button.clicked.connect(self.matched)
-        self.button_mismatched = QPushButton("Mismatched")
+        # mismatch button
+        self.button_mismatched = QPushButton("Mismatch")
         self.button_mismatched.clicked.connect(self.mismatched)
+        
+        # two borderline
+        self.button_match_b = QPushButton("Above borderline")
+        self.button_match_b.clicked.connect(self.match_b)
+        self.button_mismatch_b = QPushButton("Below borderline")
+        self.button_mismatch_b.clicked.connect(self.mismatch_b)
 
         #self.text = QLabel("Dear Annotator, <font color='red'>WELCOME!</font><br>Example: The man is sleepling on the chair")
         self.text = QLabel("Dear Annotator, <font color='red'>WELCOME!</font><br>Example: Young people are riding through the steel forest.")
         self.text.setAlignment(QtCore.Qt.AlignCenter)
         self.main_layout.addWidget(self.text)
         self.main_layout.addWidget(self.button)
+        self.main_layout.addWidget(self.button_match_b)
+        self.main_layout.addWidget(self.button_mismatch_b)
         self.main_layout.addWidget(self.button_mismatched)
 
         self.setLayout(self.main_layout)  # set layot
@@ -71,8 +88,8 @@ class ImageViewer(QWidget):
 
     def annotate(self, idx, value, d_info):
         # append new annotation to csv file
-        with jsonlines.open(self.out_file, mode='a') as writer:
-            d_info.update({"matched": value})
+        with jsonlines.open(self.out_file+f'.{value}', mode='a') as writer:
+            d_info.update({"match level": value})
             writer.write(d_info)
 
     @QtCore.pyqtSlot()
@@ -86,15 +103,49 @@ class ImageViewer(QWidget):
             self.image_label.setPixmap(QPixmap(osp.join(self.annt_path, image_file)).scaledToHeight(420))
             self.image_label.setAlignment(QtCore.Qt.AlignCenter)
             if self.cnt != 0:
-                self.annotate(self.cnt, True, obj)
+                self.annotate(self.cnt, 1, obj)
             self.cnt += 1
         except:
             self.reader.close()
             self.text.setText("End of file, please close the windows")
             
     @QtCore.pyqtSlot()
+    def match_b(self):
+        try:
+            obj = self.reader.read()
+            image_file = obj['image_file']
+            self.text.setText(self.format_text(obj))
+            self.text.setAlignment(QtCore.Qt.AlignLeft)
+
+            self.image_label.setPixmap(QPixmap(osp.join(self.annt_path, image_file)).scaledToHeight(420))
+            self.image_label.setAlignment(QtCore.Qt.AlignCenter)
+            if self.cnt != 0:
+                self.annotate(self.cnt, 2, obj)
+            self.cnt += 1
+        except:
+            self.reader.close()
+            self.text.setText("End of file, please close the windows")
+
+    @QtCore.pyqtSlot()
+    def mismatch_b(self):
+        try:
+            obj = self.reader.read()
+            image_file = obj['image_file']
+            self.text.setText(self.format_text(obj))
+            self.text.setAlignment(QtCore.Qt.AlignLeft)
+
+            self.image_label.setPixmap(QPixmap(osp.join(self.annt_path, image_file)).scaledToHeight(420))
+            self.image_label.setAlignment(QtCore.Qt.AlignCenter)
+            if self.cnt != 0:
+                self.annotate(self.cnt, 3, obj)
+            self.cnt += 1
+        except:
+            self.reader.close()
+            self.text.setText("End of file, please close the windows")
+
+    @QtCore.pyqtSlot()
     def mismatched(self):
-        # try:
+        try:
             obj = self.reader.read()
             image_file = obj['image_file']
             self.text.setText(self.format_text(obj))
@@ -102,11 +153,11 @@ class ImageViewer(QWidget):
             self.image_label.setPixmap(QPixmap(osp.join(self.annt_path, image_file)).scaledToHeight(420))
             self.image_label.setAlignment(QtCore.Qt.AlignCenter)
             if self.cnt != 0:
-                self.annotate(self.cnt, False, obj)
+                self.annotate(self.cnt, 4, obj)
             self.cnt += 1
-        # except:
-        #     self.reader.close()
-        #     self.text.setText("End of file, please close the windows")
+        except:
+            self.reader.close()
+            self.text.setText("End of file, please close the windows")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
